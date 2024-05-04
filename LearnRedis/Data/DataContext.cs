66d6916e11d616022fb852f0be845e3a031cb15d
Abhinav Kumar <dynamic2106@gmail.com>
2024-05-04 21:52:59 +0530

    Added Efcore

---
+++ LearnRedis/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using LearnRedis.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearnRedis.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions options):base(options)
+        {
+            
+        }
+        public DbSet<Employee> Employees { get; set; }  
+    }
+}
